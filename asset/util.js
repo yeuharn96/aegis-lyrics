5d575d35b7e7dbfe983fa87fe0c96dbc6666a692
YH <yeu.harn@hotmail.com>
2022-04-26 16:45:45 +0800

    initial commit

--- asset/util.js
+++
@@ -1,4 +0,0 @@
-Number.prototype.round = function (precision) {
-    const num = Math.round((this.valueOf() + Number.EPSILON) * Math.pow(10, precision)) / Math.pow(10, precision);
-    return num.toFixed(precision);
-}
